7e07c1693a778786aae1d8afc89742abf9718613
cjh3576 <48780830+cjh3576@users.noreply.github.com>
2019-03-21 17:52:10 +0900

    Day04
    3/21

--- src/com/JH/array1/ArrayTest_6.java
+++ src/com/JH/array1/ArrayTest_6.java
@@ -18,12 +18,12 @@ public class ArrayTest_6 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String [] name = null; //학생번호
-		int [] number = null;// 학생 이름
+		String [] name = null; //학생이름
+		int [] number = null;// 학생 번호
 		int [] kor = null; // 국어점수
 		int [] eng = null;// 영어점수
 		int [] math = null; // 수학점수
-		
+		int stuNum = 0; //학생 수
 		
 		boolean check = true;
 		while (check) {
@@ -37,11 +37,37 @@ public class ArrayTest_6 {
 			case 1:
 				System.out.println("학생등록");
 				System.out.println("학생 수 입력");
-				int stuNum = sc.nextInt();
+				stuNum = sc.nextInt();
 				
-				for(int i = 0; i<)
 				
-				for(int i=0; )
+				name = new String[stuNum];
+				number = new int[stuNum];
+				kor = new int[stuNum];
+				eng = new int[stuNum];
+				math = new int[stuNum];
+				for(int i = 0; i<name.length; i++) {
+					System.out.println(i+1 + " 번째 학생 이름 입력");
+					name[i] = sc.next();
+					System.out.println(i+1 + " 번째 학생 번호 입력");
+					number[i] = sc.nextInt();
+					System.out.println(i+1 + " 번째 학생 국어 점수 입력");
+					kor[i] = sc.nextInt();
+					System.out.println(i+1 + " 번째 학생 영어 점수 입력");
+					eng[i] = sc.nextInt();
+					System.out.println(i+1 + " 번째 학생 수학 점수 입력");
+					math[i] = sc.nextInt();
+				}//1. 학생 등록 끝
+				break;
+			case 2:
+				System.out.println("전체정보 조회");
+				for(int i=0; i<)
+				
+				
+				
+				
+				
+				
+				
 				
 			
 			
